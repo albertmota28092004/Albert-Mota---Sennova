e69cff3882421dfe2cd0b4753b3d3a07992fd249
albertmota28092004 <argenisgaray28@gmail.com>
2024-04-24 16:07:04 -0500

    comiteado boom perro

---
+++ banco/conexion.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class Conexion():
+    def __init__(self):
+        # Craer base de datos
+        try:
+            self.con = sqlite3.connect("banco.db")
+            self.crearTablas()
+        except Exception as ex:
+            print(ex)
+
+    def crearTablas(self):
+        sql_create_table1 = """ CREATE TABLE IF NOT EXISTS usuarios(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT,  
+            usuario TEXT UNIQUE, 
+            clave TEXT)"""
+        cur = self.con.cursor()
+        cur.execute(sql_create_table1)
+        cur.close()
+        self.crearAdmin()
+
+    def crearAdmin(self):
+        try:
+            sql_insert = """ INSERT INTO usuarios VALUES(null,'{}', '{}', '{}')""".format("Administrador", "admin", "admin2809.")
+            cur = self.con.cursor()
+            cur.execute(sql_insert)
+            self.con.commit()
+        except Exception as ex:
+            print("Ya se creó el usuario admin", ex)
+
+con = Conexion()
